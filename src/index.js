ced4781c4c8a01dc6eeda3ced17596ed3d16db45
Reese Sorrell <joreesesorrell@gmail.com>
2023-02-24 10:56:20 -0600

    Finish functionality and add 3 second interval

--- src/index.js
+++ src/index.js
@@ -1,44 +1,55 @@
 import displayAdder from "display-adder-reese";
-import major1 from "./img/major1.jpeg";
-import major2 from "./img/major5.jpeg";
-import major3 from "./img/major3.jpeg";
+import major2 from "./img/major1.jpeg";
+import major3 from "./img/major5.jpeg";
+import major1 from "./img/major3.jpeg";
 import major4 from "./img/major4.jpeg";
 
 const makeFirstScreen = () => {
     const main = document.getElementById('main');
+    displayAdder.createDiv(main, 'Hey Major', 'header-text');
     const photoHolder = displayAdder.createDiv(main, '', 'photo-holder');
     photoHolder.dataset.iterationNum = 0;
+    //displayAdder.createButton(photoHolder, rotatePhotos, 'Lets go', 'rotate-button');
     displayAdder.createImage(photoHolder, major1, '', 'major-picture,entering-photo');
-    displayAdder.createImage(photoHolder, major2, '', 'major-picture');
+    displayAdder.createImage(photoHolder, major2, '', 'major-picture,main-picture');
     displayAdder.createImage(photoHolder, major3, '', 'major-picture,exiting-photo');
-    displayAdder.createButton(photoHolder, rotatePhotos, 'Lets go', 'rotate-button');
 }
 
 function rotatePhotos() {
     var photoArray = [major1, major2, major3, major4];
-    var onScreenPhotos = document.getElementsByClassName('major-picture');
-    console.log(onScreenPhotos.length);
-    for(let i = 0; i < onScreenPhotos.length; i++) {
-        toggleClasses(onScreenPhotos[i]);
+    const onScreenPhotos = document.getElementsByClassName('major-picture');
+    var leavingPhoto;
+    for(let i = 0; i < 3; i++) {
+        var potentialLeavingPhoto = toggleClasses(onScreenPhotos[i]);
+        if(potentialLeavingPhoto) {
+            leavingPhoto = potentialLeavingPhoto;
+        }
     }
+    leavingPhoto.classList.remove('exiting-photo');
+    leavingPhoto.classList.add('entering-photo');
+    setTimeout(() => {
+    leavingPhoto.remove();
     const iterationNum = parseInt(document.getElementById('photo-holder').dataset.iterationNum);
     var newPhotoNum = (iterationNum + 3) % photoArray.length;
     const photoHolder = document.getElementById('photo-holder');
     displayAdder.createImage(photoHolder, photoArray[newPhotoNum], '', 'major-picture,entering-photo');
-
-    document.getElementById('photo-holder').dataset.iterationNum = iterationNum + 1;
+    document.getElementById('photo-holder').dataset.iterationNum = iterationNum + 1;}, 200)
 }
 
 function toggleClasses(photo) {
     if(photo.classList[1] == 'entering-photo') {
         photo.classList.remove('entering-photo');
+        photo.classList.add('main-picture')
     }
     else if(photo.classList[1] == 'exiting-photo') {
-       photo.remove(); 
+       return photo;
     }
     else {
         photo.classList.add('exiting-photo');
+        photo.classList.remove('main-picture');
     }
 }
 
 makeFirstScreen();
+
+setInterval(() => {rotatePhotos()}, 3000);
